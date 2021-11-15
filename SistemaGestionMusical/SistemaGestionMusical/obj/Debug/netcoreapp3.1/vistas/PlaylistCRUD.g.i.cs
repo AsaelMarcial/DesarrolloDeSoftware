46428dfeffb5530c387039f3b9b547b8d3e55645
Dltunas <dltunasd@gmail.com>
2021-11-14 20:22:51 -0600

    Interfaces alfredo
    Interfaces de álbum, canción y artista

--- SistemaGestionMusical/SistemaGestionMusical/obj/Debug/netcoreapp3.1/vistas/PlaylistCRUD.g.i.cs
+++ SistemaGestionMusical/SistemaGestionMusical/obj/Debug/netcoreapp3.1/vistas/PlaylistCRUD.g.i.cs
@@ -47,13 +47,13 @@ namespace SistemaGestionMusical.vistas {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SistemaGestionMusical;component/vistas/playlistcrud.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SistemaGestionMusical;V1.0.0.0;component/vistas/playlistcrud.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\vistas\PlaylistCRUD.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace SistemaGestionMusical.vistas {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
