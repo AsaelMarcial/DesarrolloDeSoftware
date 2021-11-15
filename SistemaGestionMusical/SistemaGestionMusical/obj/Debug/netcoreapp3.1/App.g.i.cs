efc541af45b0dafb4924383fccfb10109f02dc7b
Syer <israzavriv@gmail.com>
2021-11-14 22:20:14 -0600

    ward

--- SistemaGestionMusical/SistemaGestionMusical/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ SistemaGestionMusical/SistemaGestionMusical/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace SistemaGestionMusical {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace SistemaGestionMusical {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public static void Main() {
             SistemaGestionMusical.App app = new SistemaGestionMusical.App();
             app.InitializeComponent();
